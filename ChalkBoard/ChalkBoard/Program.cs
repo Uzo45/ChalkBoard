6620cbce785b0e0eba2909fef143915c5a776f6a
jermiatravers150 <jermiatravers150@gmail.com>
2023-12-09 17:12:50 -0500

    changing database location

--- ChalkBoard/ChalkBoard/Program.cs
+++ ChalkBoard/ChalkBoard/Program.cs
@@ -11,7 +11,15 @@ namespace ChalkBoard
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
+<<<<<<< Updated upstream
             Application.Run(new Form1());
+=======
+
+            Info.dbConnection = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\Users\\After Grad Party\\OneDrive - Bowie State\\COSC 465\\ChalkBoard\\ChalkBoardDatabase.accdb");
+            //Info.dbConnection.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=D:\\School\\Chalkboard\\ChalkBoardDatabase.accdb";
+            Info.dbConnection.Open();
+            Application.Run(new UserType());
+>>>>>>> Stashed changes
         }
     }
 }
